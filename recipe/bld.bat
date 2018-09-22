f58578576b58aacfac83e8fc318c22cb2d0c491e
Daniel Bast <2790401+dbast@users.noreply.github.com>
2018-09-22 20:39:51 +0200

    Change bld.bat comment style from rem to ::

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,18 +1,18 @@
-rem go one level up
+:: go one level up
 cd %SRC_DIR%
 cd ..
 
-rem create the gopath directory structure
+:: create the gopath directory structure
 set "GOPATH=%CD%\gopath"
 mkdir "%GOPATH%\src\github.com\jesseduffield" || goto :error
 mklink /D "%GOPATH%\src\github.com\jesseduffield\lazygit" "%SRC_DIR%" || goto :error
 cd "%GOPATH%\src\github.com\jesseduffield\lazygit"
 
-rem build the project
+:: build the project
 go get -v || goto :error
 go build || goto :error
 
-rem install the binary
+:: install the binary
 mv "%GOPATH%\bin\lazygit" "%LIBRARY_BIN%\lazygit" || goto :error
 goto :EOF
 
